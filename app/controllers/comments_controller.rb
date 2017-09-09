9d657bc695cbe6585cc942b3d28ecf79ded29fd0
Yonga <jorgeggayon@gmail.com>
2017-09-09 12:23:15 -0500

    Creating and updating commets from article show

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,39 @@
 class CommentsController < ApplicationController
+
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = Comment.new(comment_params)
+    @comment.article = @article
+    if @comment.save
+      redirect_to article_path @article
+    else
+      render '/articles/show'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:article_id])
+    @updated_comment = @article.comments.find(params[:id])
+    if @updated_comment.update(comment_params)
+      redirect_to article_path(@article)
+    else
+      render '/articles/show'
+    end
+  end
+
+
+
+
+
+
+
+
+
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
+  end
+
 end
